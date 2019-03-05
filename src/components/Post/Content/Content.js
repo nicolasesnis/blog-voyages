9c826529cd018433015fa9918fda7228b9d16fe3
Nicolas Esnis <nicolas.esnis@gmail.com>
2019-03-05 16:28:56 +0100

    applause button

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import kebabCase from 'lodash/kebabCase';
+
 import styles from './Content.module.scss';
 
 const Content = ({ body, title, country }) => (
@@ -9,6 +10,7 @@ const Content = ({ body, title, country }) => (
     <div className={styles['content__subtitle']}>
       <Link to={`/country/${kebabCase(country)}/`}>{country}</Link>
     </div>
+
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
   </div>
 );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import script from 'applause-button';
 import Author from './Author';
 import Comments from './Comments';
 import Content from './Content';
 import Meta from './Meta';
 import Tags from './Tags';
 import styles from './Post.module.scss';
+import applauseStyles from './applause-button.css';
 
 const Post = ({ post }) => {
   const {
@@ -27,7 +29,13 @@ const Post = ({ post }) => {
 
       <div className={styles['post__footer']}>
         <Meta date={date} />
+
         <Tags tags={tags} tagSlugs={tagSlugs} />
+        <applause-button
+          color="black"
+          className={applauseStyles}
+          style={{ width: '50px', height: '50px', margin: 'auto' }}
+        />
         <Author />
       </div>
 
