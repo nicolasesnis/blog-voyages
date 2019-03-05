6f7c749c1075ee6b596bb66ecfa8eacc72392b72
Nicolas Esnis <nicolas.esnis@gmail.com>
2019-03-05 16:39:28 +0100

    applause button

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,3 +2,19 @@
 
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
+
+// Ajouté par Nico pour fix le package applause-button
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /applause-button/,
+            use: loaders.null()
+          }
+        ]
+      }
+    });
+  }
+};
